d29d9603b3ed08957f35dc4459c4001295a7ea06
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-02-27 00:46:53 +0300

    hw1_z1

---
+++ hw1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "********" << std::endl
+             << "       *" << std::endl
+             << "      * " << std::endl
+             << "     *  " << std::endl
+             << "    *   " << std::endl
+             << "   *    " << std::endl
+             << "  *     " << std::endl
+             << " *      " << std::endl
+             << "********" << std::endl;
+
+   std::getchar();
+   std::getchar();
+
+   return 0;
+}
