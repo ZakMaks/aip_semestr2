6fda1e81ba450b3e99808e85b81ff2f0bad4d645
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-02-27 00:49:51 +0300

    hw1_z5

---
+++ hw5/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+//zuducha 9-B
+
+int main()
+{
+    int  a;
+
+        std::cout << "VVedi velichinu ";
+        std::cin >> a;
+        std::cout << a << " L = "<< a * 0.001<<" m3 "<< std::endl;
+        std::cout << a << " L = "<< a * 0.0063<<" barrel "<< std::endl;
+        std::cout << a << " m3 = "<< a * 1000<<" L "<< std::endl;
+        std::cout << a << " m3 = "<< a * 6.29<<" barrel "<< std::endl;
+        std::cout << a << " B = "<< a * 0.16<<" m3 "<< std::endl;
+        std::cout << a << " B = "<< a * 158.99<<" L "<< std::endl;
+
+    std::getchar();
+    std::getchar();
+    return 0;
+}
+
+
+
