b1f5a3103029efe03e9622442004e15283df1240
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-02-27 00:49:22 +0300

    hw1_z4

---
+++ hw4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+// zuducha 8-B
+
+int main()
+{
+    int x, F;
+    x = -2;
+    F = abs (x) + pow(x,5);
+    std::cout << "funkcia F =  " << F;
+    return 0;
+}
+
+
+
