bb013566b0443cc3fc550f412091616c9e583840
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-03-22 01:38:05 +0300

    hw2 z2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
-#include <iostream>
+#include <iostream> //nomer 2b
 #include <fstream>
 #include <string>
-//Вывести на экран Первую букву имени из символов "*" (звездочка)
+
+//Пользователь вводит параметры фигуры. Найти периметр и площадь фигур:
+//b. Прямоугольник
 
 int main()
 {
-   std::cout << "********" << std::endl
-             << "       *" << std::endl
-             << "      * " << std::endl
-             << "     *  " << std::endl
-             << "    *   " << std::endl
-             << "   *    " << std::endl
-             << "  *     " << std::endl
-             << " *      " << std::endl
-             << "********" << std::endl;
+    int a , b;
+    std::cout << "Vvedi storonu prumougolnika A " << std::endl;
+    std::cin >> a;
 
-   std::getchar();
-   std::getchar();
-   return 0;
+    std::cout << "Vvedi storonu prumougolnika B " << std::endl;
+    std::cin >> b;
+
+    std::cout << " P = " << (a + b)*2 << std::endl;
+    std::cout << " S = " << a * b << std::endl;
+
+    std::getchar();
+    std::getchar();
+
+    std::fstream f("Otvet.txt", std::ios::out); // открываем для записи
+    f << "P = " << (a + b)*2<< "  "<< " S = "<< a * b ;
+     f.close(); // закрываем файл
+
+    return 0;
 }
-
-
 
 
 
