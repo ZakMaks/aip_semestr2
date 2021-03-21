a84f70403b85355e18133068eeb0842bce06f65f
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-03-22 01:37:12 +0300

    hw2 z1

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+//Вывести на экран Первую букву имени из символов "*" (звездочка)
+
+int main()
+{
+   std::cout << "********" << std::endl
+             << "       *" << std::endl
+             << "      * " << std::endl
+             << "     *  " << std::endl
+             << "    *   " << std::endl
+             << "   *    " << std::endl
+             << "  *     " << std::endl
+             << " *      " << std::endl
+             << "********" << std::endl;
+
+   std::getchar();
+   std::getchar();
+   return 0;
+}
+
+
+
+
+
