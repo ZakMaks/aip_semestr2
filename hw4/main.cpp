5d516cf5401716bec5816c2b8c4c7353beffacab
ZakMaks <79425826+ZakMaks@users.noreply.github.com>
2021-03-22 01:45:31 +0300

    hw2 z4

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <string>
 // zuducha 8-B
+//Вычислите значение выражения:
+//b. |x| + x^5, если x=−2. Ответ: -30
 
 int main()
 {
@@ -8,6 +12,11 @@ int main()
     x = -2;
     F = abs (x) + pow(x,5);
     std::cout << "funkcia F =  " << F;
+
+    std::fstream f("Otvet.txt", std::ios::out); // открываем для записи
+    f << "funkcia F =  " << F;
+     f.close(); // закрываем файл
+
     return 0;
 }
 
